5d178aa42df3965a89ae375f9c94cf90e8efb3c4
Peter Haza <peter.haza@gmail.com>
2015-03-22 19:11:26 +0000

    Use correctschema grammar

--- src/PostgisConnection.php
+++ src/PostgisConnection.php
@@ -3,4 +3,13 @@
 use Bosnadev\Database\PostgresConnection;
 
 class PostgisConnection extends PostgresConnection {
+	/**
+	 * Get the default schema grammar instance.
+	 *
+	 * @return \Illuminate\Database\Grammar
+	 */
+	protected function getDefaultSchemaGrammar()
+	{
+		return $this->withTablePrefix(new Schema\Grammars\PostgisGrammar);
+	}
 }
