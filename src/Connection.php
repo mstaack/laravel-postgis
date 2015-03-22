ae868aa66c72654a07cc5529d09f54ddf8f78f24
Peter Haza <peter.haza@gmail.com>
2015-03-22 19:03:32 +0000

    Use correct schema builder

--- src/Connection.php
+++ src/Connection.php
@@ -1,5 +1,11 @@
 <?php namespace Phaza\LaravelPostgis;
 
 class Connection extends \Bosnadev\Database\Connection {
+	public function getSchemaBuilder()
+	{
+		if (is_null($this->schemaGrammar)) { $this->useDefaultSchemaGrammar(); }
+
+		return new Schema\Builder($this);
+	}
 
 }
