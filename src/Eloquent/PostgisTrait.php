fdb781e4a7f8336c1d6b3f731e6a483f75062d41
Peter Haza <peter.haza@gmail.com>
2015-04-17 13:20:51 +0200

    Fix duplicate pivot table call

--- src/Eloquent/PostgisTrait.php
+++ src/Eloquent/PostgisTrait.php
@@ -44,14 +44,37 @@ trait PostgisTrait {
 	}
 
 	public function belongsToMany($related, $table = null, $foreignKey = null, $otherKey = null, $relation = null) {
-		/**
-		 * Illuminate\Database\Eloquent\Relations\BelongsToMany
-		 */
-		$object = parent::belongsToMany( $related, $table, $foreignKey, $otherKey, $relation );
+		// If no relationship name was passed, we will pull backtraces to get the
+		// name of the calling function. We will use that function name as the
+		// title of this relation since that is a great convention to apply.
+		if (is_null($relation))
+		{
+			$relation = $this->getBelongsToManyCaller();
+		}
 
+		// First, we'll need to determine the foreign key and "other key" for the
+		// relationship. Once we have determined the keys we'll make the query
+		// instances as well as the relationship instances we need for this.
+		$foreignKey = $foreignKey ?: $this->getForeignKey();
 
-		return new BelongsToMany($object->getQuery(), $object->getParent(), $object->getTable(), $object->getForeignKey()
-			, $object->getOtherKey(), $object->getRelationName());
+		$instance = new $related;
+
+		$otherKey = $otherKey ?: $instance->getForeignKey();
+
+		// If no table name was provided, we can guess it by concatenating the two
+		// models using underscores in alphabetical order. The two model names
+		// are transformed to snake case from their default CamelCase also.
+		if (is_null($table))
+		{
+			$table = $this->joiningTable($related);
+		}
+
+		// Now we're ready to create a new query builder for the related model and
+		// the relationship instances for the relation. The relations will set
+		// appropriate query constraint and entirely manages the hydrations.
+		$query = $instance->newQuery();
+
+		return new BelongsToMany($query, $this, $table, $foreignKey, $otherKey, $relation);
 	}
 
 
