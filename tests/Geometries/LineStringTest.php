d9fb3475903a89426b63a6addff6adc85192ee90
MetalMatze <me@metalmatze.de>
2015-05-06 15:17:11 +0200

    Convert all files in tests/ to psr-2 conform files

--- tests/Geometries/LineStringTest.php
+++ tests/Geometries/LineStringTest.php
@@ -1,30 +1,32 @@
 <?php
 
-use Phaza\LaravelPostgis\Geometries\Geometry;
 use Phaza\LaravelPostgis\Geometries\LineString;
 use Phaza\LaravelPostgis\Geometries\Point;
 
-class LineStringTest extends BaseTestCase {
-	public function testFromWKT()
-	{
-		$linestring = LineString::fromWKT( 'LINESTRING(0 0, 1 1, 2 2)' );
-		$this->assertInstanceOf( LineString::class, $linestring );
+class LineStringTest extends BaseTestCase
+{
+    public function testFromWKT()
+    {
+        $linestring = LineString::fromWKT('LINESTRING(0 0, 1 1, 2 2)');
+        $this->assertInstanceOf(LineString::class, $linestring);
 
-		$this->assertEquals(3, $linestring->count());
-	}
+        $this->assertEquals(3, $linestring->count());
+    }
 
-	public function testToWKT() {
+    public function testToWKT()
+    {
 
-		$points = [new Point(0,0), new Point(1,1), new Point(2,2)];
-		$linestring = new LineString($points);
+        $points = [new Point(0, 0), new Point(1, 1), new Point(2, 2)];
+        $linestring = new LineString($points);
 
-		$this->assertEquals( 'LINESTRING(0 0,1 1,2 2)', $linestring->toWKT() );
-	}
+        $this->assertEquals('LINESTRING(0 0,1 1,2 2)', $linestring->toWKT());
+    }
 
-	public function testToString() {
-		$points = [new Point(0,0), new Point(1,1), new Point(2,2)];
-		$linestring = new LineString($points);
+    public function testToString()
+    {
+        $points = [new Point(0, 0), new Point(1, 1), new Point(2, 2)];
+        $linestring = new LineString($points);
 
-		$this->assertEquals('0 0,1 1,2 2', (string)$linestring);
-	}
+        $this->assertEquals('0 0,1 1,2 2', (string)$linestring);
+    }
 }
--- tests/Schema/BuilderTest.php
+++ tests/Schema/BuilderTest.php
@@ -6,17 +6,18 @@ use Phaza\LaravelPostgis\PostgisConnection;
 use Phaza\LaravelPostgis\Schema\Builder;
 use Phaza\LaravelPostgis\Schema\Blueprint;
 
-class BuilderTest extends BaseTestCase {
-	public function testReturnsCorrectBlueprint()
-	{
-		$connection = Mockery::mock( PostgisConnection::class );
-		$connection->shouldReceive('getSchemaGrammar')->once()->andReturn(null);
+class BuilderTest extends BaseTestCase
+{
+    public function testReturnsCorrectBlueprint()
+    {
+        $connection = Mockery::mock(PostgisConnection::class);
+        $connection->shouldReceive('getSchemaGrammar')->once()->andReturn(null);
 
-		$mock = Mockery::mock( Builder::class, [ $connection ] );
-		$mock->makePartial()->shouldAllowMockingProtectedMethods();
-		$blueprint = $mock->createBlueprint('test', function() {});
+        $mock = Mockery::mock(Builder::class, [$connection]);
+        $mock->makePartial()->shouldAllowMockingProtectedMethods();
+        $blueprint = $mock->createBlueprint('test', function () {
+        });
 
-		$this->assertInstanceOf( Blueprint::class, $blueprint );
-	}
-
+        $this->assertInstanceOf(Blueprint::class, $blueprint);
+    }
 }
