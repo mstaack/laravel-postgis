46123939bd990ffe0c1ecf48f95144d4a94de3e1
Peter Mein <peter@2mein.nl>
2015-05-07 13:21:04 +0200

    Fixed format for storing Polygons

--- src/Geometries/Polygon.php
+++ src/Geometries/Polygon.php
@@ -2,10 +2,19 @@
 
 use Countable;
 
-class Polygon extends MultiLineString implements Countable {
+class Polygon extends MultiLineString implements Countable
+{
 
-	public function toWKT()
-	{
-		return sprintf( 'POLYGON(%s)', (string) $this );
-	}
+    public function toWKT()
+    {
+        return sprintf('POLYGON((%s))', (string)$this);
+    }
+
+
+    public function __toString()
+    {
+        return implode( ',', array_map( function ( LineString $linestring ) {
+            return sprintf( '%s', (string) $linestring );
+        }, $this->getLineStrings() ) );
+    }
 }
