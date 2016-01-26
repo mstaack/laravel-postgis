ee8a5a2975ccf0e67d6ea574852cb103fa1159f7
Peter Haza <peter.haza@gmail.com>
2016-01-26 08:33:00 +0000

    Add support for Laravel 5.2

--- src/Connectors/ConnectionFactory.php
+++ src/Connectors/ConnectionFactory.php
@@ -6,14 +6,14 @@ use Phaza\LaravelPostgis\PostgisConnection;
 class ConnectionFactory extends \Bosnadev\Database\Connectors\ConnectionFactory
 {
     /**
-     * @param string $driver
-     * @param PDO $connection
-     * @param string $database
-     * @param string $prefix
-     * @param array $config
-     * @return mixed|PostgisConnection
+     * @param string       $driver
+     * @param \Closure|PDO $connection
+     * @param string       $database
+     * @param string       $prefix
+     * @param array        $config
+     * @return \Illuminate\Database\Connection|PostgisConnection
      */
-    protected function createConnection($driver, PDO $connection, $database, $prefix = '', array $config = [])
+    protected function createConnection($driver, $connection, $database, $prefix = '', array $config = [])
     {
         if ($this->container->bound($key = "db.connection.{$driver}")) {
             return $this->container->make($key, [$connection, $database, $prefix, $config]);
