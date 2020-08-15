87f252fa130b845d7b1149471df6b5020029a4b5
Tomáš Vlk <tomas@vlktomas.cz>
2020-08-16 01:22:25 +0200

    fix connecting to database when registering package

--- src/PostgisConnection.php
+++ src/PostgisConnection.php
@@ -7,15 +7,6 @@ use MStaack\LaravelPostgis\Schema\Grammars\PostgisGrammar;
 
 class PostgisConnection extends PostgresConnection
 {
-    public function __construct($pdo, $database = '', $tablePrefix = '', array $config = [])
-    {
-        parent::__construct($pdo, $database, $tablePrefix, $config);
-
-        // Prevent geography type fields from throwing a 'type not found' error.
-        $this->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('geography', 'string');
-        $this->getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('geometry', 'string');
-    }
-
     /**
      * Get the default schema grammar instance.
      *
