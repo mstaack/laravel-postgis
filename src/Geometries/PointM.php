7f152cfae45791c6a204fe3b1143aa6cdceb7fec
Bernard Diphooren <bernard@appnormal.com>
2022-08-02 13:30:40 +0200

    feat: update PointM to use measure, x, y and z in the constructor

--- src/Geometries/PointM.php
+++ src/Geometries/PointM.php
@@ -6,10 +6,40 @@ class PointM extends Point
 {
     protected $measure;
 
-    public function __construct($lat, $lng, $measure, $alt = null)
+    public function __construct($measure, $x, $y, $z = null)
     {
-        parent::__construct($lat, $lng, $alt);
+        parent::__construct($y, $x, $z);
         $this->measure = (float) $measure;
+    }
+
+    public function setX($x)
+    {
+        $this->setLng($x);
+    }
+
+    public function getX()
+    {
+        return $this->getLng();
+    }
+
+    public function setY($y)
+    {
+        $this->setLat($y);
+    }
+
+    public function getY()
+    {
+        return $this->getLat();
+    }
+
+    public function getZ()
+    {
+        return $this->getAlt();
+    }
+
+    public function setZ($z)
+    {
+        $this->setAlt($z);
     }
 
     public function setMeasure($measure)
@@ -27,7 +57,12 @@ class PointM extends Point
         $wktType = parent::toWKT();
         $insertBefore = mb_strpos($wktType, '(');
 
-        return substr_replace($wktType, 'M', $insertBefore, 0);
+        return substr_replace(
+            $wktType,
+            $this->is3d() ? 'M' : ' M',
+            $insertBefore,
+            0
+        );
     }
 
     public function toPair()
@@ -40,12 +75,12 @@ class PointM extends Point
         $pair = preg_replace('/^[a-zA-Z\(\)]+/', '', trim($pair));
         $splits = explode(' ', trim($pair));
         $measure = array_pop($splits);
-        $lng = $splits[0];
-        $lat = $splits[1];
+        $x = $splits[0];
+        $y = $splits[1];
         if (count($splits) > 2) {
-            $alt = $splits[2];
+            $z = $splits[2];
         }
 
-        return new static((float) $lat, (float) $lng, (float) $measure, isset($alt) ? (float) $alt : null);
+        return new static((float) $measure, (float) $x, (float) $y, isset($z) ? (float) $z : null);
     }
 }
